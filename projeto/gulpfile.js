953c1a9b5ea7fe4b2140850b2ad19d7f625d90a5
Elio <laenderquadros@gmail.com>
2019-05-29 08:25:37 -0300

    concat de scripts

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,13 +1,13 @@
 // Chamada na lib do gulp
 const gulp = require("gulp");
 // Chamada na extensão para minificar imagem do gulp
-const imgMin = require("gulp-imagemim");
+const imgMin = require("gulp-imagemin");
 // Chamada na extensão responsável por excluir os arquivos de um dado diretório
 const clean = require("gulp-clean");
 // npm install gulp concat --save-dev
 const concat = require("gulp-concat"); // Realiza a concatenação de scripts
 // npm install gulp-html-replace
-const htmlReplace = require("gul-html-replace"); // Substitui as tags de importação após concatenação.
+const htmlReplace = require("gulp-html-replace"); // Substitui as tags de importação após concatenação.
 
 /**
  * O gulp funciona a partir de tarefas, que são scripts contendo pipes com instruções para tratar os arquivos.
@@ -16,6 +16,13 @@ const htmlReplace = require("gul-html-replace"); // Substitui as tags de importa
  * Ex de chamada dessa task: 'npm run gulp build-img'
  * Lembrando é uma boa prática anexar o gulp no campo scripts do package json, para acessarmos com maior facilidade.
  */
+
+// Tarefa padrão que será executada por default caso não seja passado o nome de uma tarefa ao executar o gulp no terminal
+gulp.task('default', ['copy'], () => {
+  // Realiza chamada nas tasks passadas por parâmetro PS: Essas tasks não possui return na implementação, assim sendo, serão 
+  // executadas de forma assíncrona, sem ordem de um para outra. Obs: quando tem return, é tratada como promisse. 
+  gulp.start('build-img', 'build-js', 'build-html');
+}); 
 
 /**
  * É possível listar as tarefas que devem ser executadas antes da task que foi chamada. Basta adicionar o nome das demais tasks que deverão ser executadas
@@ -31,7 +38,7 @@ gulp.task("clean", () => {
   return gulp.src("dist").pipe(clean());
 });
 
-gulp.task("build-img", ["copy"], function() {
+gulp.task("build-img", function() {
   gulp
     .src("dist/img/**/*") // Local dos arquivos a serem tratados. Glob pattern '**':todos os diretórios a partir do que foi passado. '*':arquivos de todas extensões.
     .pipe(imagemin()) // Pipe passando as os arquivos na minificaçao
@@ -40,9 +47,10 @@ gulp.task("build-img", ["copy"], function() {
 
 gulp.task("build-js", () => {
   gulp
-    .src("dist/js/**/*.js")
+    // Para que não ocorra erro de concatenação, devemos definir a ordem dos scripts 
+    .src(["dist/js/jquery.js", "dist/js/home.js", "dist/js/produto.js"])
     .pipe(concat("all.js")) // Nome do arquivo com a concatenação de todo js encontrado.
-    .pipe(gulp.dest("dist/img"));
+    .pipe(gulp.dest("dist/js"));
 });
 
 /**
@@ -57,7 +65,7 @@ gulp.task("build-html", () => {
     .src("dist/js/**/*.html")
     .pipe(
       htmlReplace({
-        js: "all.js" // Procuta em todos htmls pela build js e substitui pela importação de 'all.js'
+        js: "js/all.js" // Procuta em todos htmls pela build js e substitui pela importação de 'all.js'
       })
     )
     .pipe(gulp.dest("dist"));
