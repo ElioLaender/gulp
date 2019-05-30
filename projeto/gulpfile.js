3fd87ab04c0bbccd7cda2c7b0b759d20e817e3c5
Elio <laenderquadros@gmail.com>
2019-05-30 08:27:45 -0300

    jshint, csshint, prefixadores, precompiladores

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -5,15 +5,27 @@ const imgMin = require("gulp-imagemin");
 // Chamada na extensão responsável por excluir os arquivos de um dado diretório
 const clean = require("gulp-clean");
 // Realiza a concatenação de scripts
-const concat = require("gulp-concat");
+// const concat = require("gulp-concat");
 // Substitui as tags de importação após concatenação.
-const htmlReplace = require("gulp-html-replace");
+// const htmlReplace = require("gulp-html-replace");
 // Minifica os scripts JS (retira espaços e quebras de linhas)
 const uglify = require("gulp-uglify");
 // Para minificação do CSS
 const cssMin = require("gulp-cssmin");
 // Facilita a troca de tags após contatenação/minificação
 const useMin = require("gulp-usemin");
+// Cria um servidor para rodar a aplicação
+const browserSync = require("browser-sync");
+// Responsável por verificar erros de sintazes no JS
+const jsHint = require("gulp-jshint");
+// Responsável por melhorar a forma como erros são descritos no terminal pelo jshint
+const jsHintStylish = require("gulp-jshintStylish");
+// Verifica erros de sintaxe no css
+const cssHint = require("gulp-csslint");
+// Realiza prefixação dos atributos exprerimentais nos nagegadores
+const autoPrefixer = require("gulp-autoprefixer");
+// Responsável por automatizar compilação de arquivos less
+const less = require('gulp-less');
 
 
 /**
@@ -23,6 +35,43 @@ const useMin = require("gulp-usemin");
  * Ex de chamada dessa task: 'npm run gulp build-img'
  * Lembrando é uma boa prática anexar o gulp no campo scripts do package json, para acessarmos com maior facilidade.
  */
+
+
+gulp.task("server", () => {
+  browserSync.init({
+    baseDir: 'src' // Seta o diretório raiz que o servidor irá apontar quando ativo.  
+  });
+
+  // Escuca alterações no JS e notifica erros de sintaxe.
+  gulp.watch("src/**/*.js")
+    .on("change", (event) => {
+      gulp.src(event.path) // retorna o caminho do arquivo que disparou o evento.
+        .pipe(jsHint()) // Verifica sintaxe js dos scripts modificados
+        .pipe(jsHint.reporter(jsHintStylish));
+    })
+
+   // Escuca alterações no CSS e notifica erros de sintaxe.
+   gulp.watch("src/**/*.css")
+   .on("change", (event) => {
+     gulp.src(event.path) // retorna o caminho do arquivo que disparou o evento.
+       .pipe(cssHint()) // Verifica sintaxe js dos scripts modificados
+       .pipe(cssHint.reporter());
+   })  
+
+  // Compila arquivos do tipo less quando ocorre mudanças no mesmo. 
+  gulp.watch('src/less/**/*.less').on('change', function(event) {
+    var stream = gulp.src(event.path)
+         .pipe(less().on('error', (erro) => { // Tratando erro, pois um erro não capturado de um stream derruba todos os outros! (server para)
+            console.log('LESS, erro compilação: ' + erro.filename);
+           console.log(erro.message);
+         }))
+         .pipe(gulp.dest('src/css'));
+ });
+
+  // Função nativa do gulp para escutar alterações nos scripts. 
+  gulp.watch("src/**/*") // Escuta todos os arquivos dentro de src
+    .on("change", browserSync.reload); // Caso o evento change for disparado recarrega a página. 
+});
 
 // Tarefa padrão que será executada por default caso não seja passado o nome de uma tarefa ao executar o gulp no terminal
 gulp.task('default', ['copy'], () => {
@@ -56,7 +105,7 @@ gulp.task("usemin", () => {
   gulp.src('dist/**/*.html')
     .pipe(useMin({
       'js': [uglify],
-      'css': [cssMin]
+      'css': [autoPrefixer, cssMin]
     }))
     .pipe(gulp.dest('dist'));
 });
